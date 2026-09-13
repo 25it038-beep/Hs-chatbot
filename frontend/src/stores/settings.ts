1cbed4e66945d2bd5d66e864c6e3dbeec3a06bf1
BS.Harshan seliyan <bs.harshan.seliyan@gmail.com>
2026-09-13 18:15:55 +0530

    frontend: add browserAutomationPriority to SettingsState to fix TS build

--- frontend/src/stores/settings.ts
+++ frontend/src/stores/settings.ts
@@ -3,17 +3,21 @@ import { create } from 'zustand'
 interface SettingsState {
   sidebarOpen: boolean
   settingsOpen: boolean
+  browserAutomationPriority: boolean
   toggleSidebar: () => void
   toggleSettings: () => void
   setSidebarOpen: (open: boolean) => void
   setSettingsOpen: (open: boolean) => void
+  setBrowserAutomationPriority: (priority: boolean) => void
 }
 
 export const useSettings = create<SettingsState>((set) => ({
   sidebarOpen: true,
   settingsOpen: false,
+  browserAutomationPriority: false,
   toggleSidebar: () => set(state => ({ sidebarOpen: !state.sidebarOpen })),
   toggleSettings: () => set(state => ({ settingsOpen: !state.settingsOpen })),
   setSidebarOpen: (open) => set({ sidebarOpen: open }),
   setSettingsOpen: (open) => set({ settingsOpen: open }),
+  setBrowserAutomationPriority: (priority) => set({ browserAutomationPriority: priority }),
 }))
